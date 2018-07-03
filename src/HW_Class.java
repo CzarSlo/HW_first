625d962e2c73050781453649a7f460d7f616fe12
Cezary Slotwinski <seezar@o2.pl>
2018-07-03 10:41:11 +0200

    Zrobienie pkt.8.

--- src/HW_Class.java
+++ src/HW_Class.java
@@ -3,5 +3,7 @@ public class HW_Class {
         System.out.println("Uczę");
         System.out.println("się");
         System.out.println("programowania");
+        System.out.println("i");
+        System.out.println("commitowania");
     }
 }
